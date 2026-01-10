5f7026ebb695ab60254d58366b263093df371f30
Dalgat Ibragimov <doly2004e@yandex.ru>
2026-01-10 14:01:56 +0300

    glolbal to class

--- lab8/8-8.cpp
+++ lab8/8-8.cpp
@@ -1,51 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
-vector<vector<int>> g;
-vector<int> side;   
-vector<int> mt;     
-vector<char> used;
+class KuhnMatching {
+public:
+    int n = 0, m = 0;
+    vector<vector<int>> g;
+    vector<int> side;     
+    vector<int> mt;        
+    vector<bool> used;   
 
-bool kuhn(int v) {
-    if (used[v]) return false;
-    used[v] = true;
-    for (int to : g[v]) {
-        if (mt[to] == -1 || kuhn(mt[to])) {
-            mt[to] = v;
-            return true;
+    bool kuhn(int v) {
+        if (used[v]) return false;
+        used[v] = true;
+        for (int to : g[v]) {
+            if (mt[to] == -1 || kuhn(mt[to])) {
+                mt[to] = v;
+                return true;
+            }
         }
-    }
-    return false;
-}
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    if (!(cin >> n >> m)) {
-        return 0;
+        return false;
     }
 
-    g.assign(n, {});
-    for (int i = 0; i < m; ++i) {
-        int u, v;
-        cin >> u >> v;
-        --u; --v;
-        g[u].push_back(v);
-        g[v].push_back(u);
-    }
+    void colorize_bfs() {
+        side.assign(n, -1);
+        for (int i = 0; i < n; ++i) {
+            if (side[i] != -1) continue;
 
-    for (int i = 0; i < n; ++i) {
-        sort(g[i].begin(), g[i].end());
-    }
-
-    side.assign(n, -1);
-    for (int i = 0; i < n; ++i) {
-        if (side[i] == -1) {
             queue<int> q;
             side[i] = 0;
             q.push(i);
+
             while (!q.empty()) {
                 int v = q.front(); q.pop();
                 for (int to : g[v]) {
@@ -58,26 +42,56 @@ int main() {
         }
     }
 
-    mt.assign(n, -1);  
-    for (int v = 0; v < n; ++v) {
-        if (side[v] == 0) {
-            used.assign(n, false);
-            kuhn(v);
+    vector<pair<int,int>> solve() {
+        colorize_bfs();
+
+        mt.assign(n, -1);
+        for (int v = 0; v < n; ++v) {
+            if (side[v] == 0) {
+                used.assign(n, false);
+                kuhn(v);
+            }
         }
+
+        vector<pair<int,int>> ans;
+        for (int v = 0; v < n; ++v) {
+            if (mt[v] != -1) {
+                int a = mt[v] + 1;
+                int b = v + 1;
+                if (a > b) swap(a, b);
+                ans.emplace_back(a, b);
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    KuhnMatching solver;
+
+    if (!(cin >> solver.n >> solver.m)) {
+        return 0;
     }
 
-    vector<pair<int,int>> ans;
-    for (int v = 0; v < n; ++v) {
-        if (mt[v] != -1) {
-            int a = mt[v] + 1;
-            int b = v + 1;
-            if (a > b) swap(a, b);
-            ans.emplace_back(a, b);
-        }
+    solver.g.assign(solver.n, {});
+    for (int i = 0; i < solver.m; ++i) {
+        int u, v;
+        cin >> u >> v;
+        --u; --v;
+        solver.g[u].push_back(v);
+        solver.g[v].push_back(u);
     }
 
-    sort(ans.begin(), ans.end());
-    
+    for (int i = 0; i < solver.n; ++i) {
+        sort(solver.g[i].begin(), solver.g[i].end());
+    }
+
+    auto ans = solver.solve();
+
     cout << ans.size() << "\n";
     for (auto &e : ans) {
         cout << e.first << " " << e.second << "\n";
